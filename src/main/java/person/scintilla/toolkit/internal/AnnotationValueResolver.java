a3d38bca996686c57cdf952e49fc8188ce3be928
scintilla <scintilla_0@icloud.com>
2025-09-29 14:05:51 +0800

    @ComponentScan

--- src/main/java/person/scintilla/toolkit/internal/AnnotationValueResolver.java
+++ src/main/java/person/scintilla/toolkit/internal/AnnotationValueResolver.java
@@ -1,17 +1,13 @@
 package person.scintilla.toolkit.internal;
 
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.util.regex.Pattern;
 
 @Component
 public class AnnotationValueResolver {
 
 	public static String resolve(String placeholder) {
 		if (!placeholder.startsWith("${")) {
-			return placeholder; // 不是占位符，直接返回
+			return placeholder;
 		}
 		String key = placeholder.substring(2, placeholder.length() - 1);
 		if ("listEmptyErrorCode".equals(key)) {
--- src/main/java/person/scintilla/toolkit/internal/ToolkitBeanAutoConfiguration.java
+++ src/main/java/person/scintilla/toolkit/internal/ToolkitBeanAutoConfiguration.java
@@ -1,17 +1,10 @@
 package person.scintilla.toolkit.internal;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import person.scintilla.toolkit.utils.SpringContextUtils;
 
 @Configuration
+@ComponentScan("person.scintilla.toolkit")
 public class ToolkitBeanAutoConfiguration {
-
-	@Bean
-	@ConditionalOnMissingBean
-	public SpringContextUtils springContextUtils() {
-		return new SpringContextUtils();
-	}
 
 }
